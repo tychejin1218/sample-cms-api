76af44fbb2be9998572957f1232eedd3f3fe9e93
jinyeongpark2 <jinyeong_park@daekyo.com>
2025-01-16 10:17:28 +0900

    Initial commit

---
+++ src/main/java/com/sample/cms/SampleCmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sample.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCmsApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleCmsApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sample/cms/SampleCmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCmsApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
